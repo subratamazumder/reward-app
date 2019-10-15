63fb0f0713d18007372208b023d9f0e7bd45ca3a
Subrata Mazumder <subrata.besu@gmail.com>
2019-10-16 01:23:25 +0530

    register screen added

---
+++ reward_app_mobile/lib/qr_generator.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'home.dart';
+
+class QRGenerator extends StatefulWidget {
+  @override
+  _QRGeneratorState createState() {
+    return _QRGeneratorState();
+  }
+}
+
+class _QRGeneratorState extends State<QRGenerator> {
+void _generateQR() {
+  print("QR done....."+nameTextController.text+mobileTextController.text);
+  setState(() {
+    tapped_on_register = true;
+    qr_status = "QR Code generate successfully";
+  });
+  
+}
+var nameTextController = TextEditingController();
+        var mobileTextController = TextEditingController();
+  String qr_status="";
+  bool tapped_on_register = false;
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: Colors.black, //or set color with: Color(0xFF0000FF)
+    ));
+    
+                return Scaffold(
+                  appBar: AppBar(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0.0,
+                      iconTheme: IconThemeData(color: Color(0xFF18D191))),
+                  body: Container(
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding:
+                              const EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+                          child: TextField(
+                            controller: mobileTextController,
+                    decoration: InputDecoration(labelText: 'Mobile Number'),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+                  child: TextField(
+                    controller: nameTextController,
+                decoration: InputDecoration(labelText: 'Name'),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 5.0, top: 10.0),
+                    child: GestureDetector(
+                      onTap: () => _generateQR(),
+                      child: Container(
+                          alignment: Alignment.center,
+                          height: 60.0,
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(9.0)),
+                          child: Text("Register Customer",
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold))),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Visibility(
+                
+                visible: tapped_on_register,
+              child : Expanded(
+                  child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 20.0, right: 5.0, top: 10.0),
+                child: Container(
+                    height: 250.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://d3uxjh2kr4vhnj.cloudfront.net/images/team/rameshwar_paul.jpg'),
+                            fit: BoxFit.cover))),
+                      
+              )))
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Visibility(
+                visible: tapped_on_register,
+              child : Expanded(
+                  child: Text(
+                            qr_status,
+                            style:  TextStyle(fontSize: 20.0, color: Colors.greenAccent, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          )))
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Visibility(
+                
+                visible: tapped_on_register,
+              child : Expanded(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 20.0, right: 5.0, top: 10.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => HomePage()));
+                    },
+                    child: Container(
+                        alignment: Alignment.center,
+                        height: 60.0,
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(9.0)),
+                        child: Text("Reset",
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold))),
+                  ),
+                ),
+              )
+              )
+            ]),
+
+            // Expanded(
+            //   child: Column(
+            //     mainAxisAlignment: MainAxisAlignment.end,
+            //     children: <Widget>[
+            //       Padding(
+            //         padding: const EdgeInsets.only(bottom:18.0),
+            //         child:  Text("Create A  Account ",style:  TextStyle(
+            //                     fontSize: 17.0, color: Color(0xFF18D191),fontWeight: FontWeight.bold)),
+            //       ),
+            //     ],
+            //   ),
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
