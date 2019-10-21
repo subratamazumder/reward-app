da14ff85d55040a60e95c2ed93fab6581dc8c9a5
Subrata Mazumder <subrata.besu@gmail.com>
2019-10-22 04:15:06 +0530

    api key,usage plan added

--- backend_service/aws/reward-registration-service/test-aws.sh
+++ backend_service/aws/reward-registration-service/test-aws.sh
@@ -1,1 +1,1 @@
-curl https://ks5k9csn0h.execute-api.ap-south-1.amazonaws.com/test/hello
+curl -H "x-api-key: 8nyBdJqwjc74dCNYomM812MvGnzxS9pJ6oaAC538" https://ks5k9csn0h.execute-api.ap-south-1.amazonaws.com/test/hello
